3a44a70ce27b2bed771e01e9a831ea3da68451d2
MarkYnot <mark727221029@gmail.com>
2023-08-02 00:20:59 +1000

    change config

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-southeast-2_za7STrHgU', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '7624tnv15010kapco366pjo849', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'ap-southeast-2_s9fMCrmN4', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'n3l93smrnj908fqqu7mclvjmi', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-southeast-2' // e.g. us-east-2
     },
     api: {
